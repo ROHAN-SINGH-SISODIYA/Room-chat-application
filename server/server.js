c77e9a85fece70a08927c572c2989d18be347780
ROHAN-SINGH-SISODIYA <rohanrajput32843@gmail.com>
2020-11-08 14:19:46 +0530

    add sockets

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname,'/../pulic')
 const port = process.env.PORT || 3000
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('A new user is connected')
+    socket.on('disconnect',()=>{
+        console.log("A user was disconnected")
+    })
+})
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`)
 })
